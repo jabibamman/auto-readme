5ebb711ea6450517dcdbae2aa6b3fb4b13c22c01
James ABIB <james.bidule@gmail.com>
2023-07-24 17:53:33 +0200

    chore: removec unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GithubModule } from './module/github.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot(), GithubModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot(), GithubModule]
 })
 export class AppModule {}
